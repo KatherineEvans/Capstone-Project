b428fd26731072d4604239063803694fe75f4729
KatherineEvans <Evans.Katherine.E@gmail.com>
2018-10-06 13:42:52 -0500

    Added Migration - bios to Users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_30_015217) do
+ActiveRecord::Schema.define(version: 2018_10_06_183952) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 2018_08_30_015217) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "phone"
+    t.string "bio"
   end
 
 end
